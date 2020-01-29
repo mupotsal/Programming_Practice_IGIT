08113bba02c04e04e2ad72ca8e0bf5ab924383c5
mupotsal <46546491+mupotsal@users.noreply.github.com>
2020-01-29 00:10:14 -0500

    Added one code for finding the missing  number

---
+++ arithmetic_progression.py
@@ -0,0 +1,14 @@
+count = 0
+for i in range(1,1000,1):
+    if i%2 != 0:
+        count +=i
+
+
+
+sum = 2
+num =2
+while num <1024:
+    sum+=num*2
+    num = num*2
+
+print(sum)
---
+++ fibonannci.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+
+price =(3,4,5,6)
+year = (2,4,8,0)
+
+plt.plot(price,year)
+
+plt.draw()
+plt.show()
